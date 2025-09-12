0aa9e131de215d6057a3a8bb51f7fa6a3de4c0eb
Darin Chambers <darinc.oss@1112.net>
2025-09-12 19:53:08 -0400

    Remove node_modules from tracking

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { GraphNetwork } from './GraphNetwork';
-import { Vector } from './Vector';
-import { Node } from './Node';
-import './styles.css';
-export default GraphNetwork;
-export { GraphNetwork, Vector, Node };
-//# sourceMappingURL=index.d.ts.map
